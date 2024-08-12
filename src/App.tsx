4a49a9ae4af7fbb8141ed929c49acd8ae86a355d
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2024-08-12 01:40:45 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ export const App = () => {
           alt=""
         />
       </div>
-      {/* <div>
+      <div>
         <img
           src="https://media.giphy.com/media/3o751Z6oPw0C69hKuY/giphy.gif?cid=790b7611nlfa6tz9xxl4y0k4lxr3gjn2s8qbjzrakm2u9ew5&ep=v1_gifs_search&rid=giphy.gif&ct=g"
           alt=""
         />
-      </div> */}
+      </div>
     </div>
   );
 };
