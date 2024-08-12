8da5771af9939511734fbe8f23de309184ada9cd
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2024-08-11 23:55:22 -0700

    test(CI/CD): make a UI change to test complete CI/CD pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ export const App = () => {
   return (
     <div className="page">
       <h1>AWS POC Sandbox</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore
-        perferendis nostrum incidunt molestias? Laboriosam blanditiis magni
-        minima quibusdam! Enim vero possimus autem aut qui consequatur odit
-        fugit minus? Maiores.
-      </p>
+      <div>
+        <img
+          src="https://media.giphy.com/media/bPCwGUF2sKjyE/giphy.gif?cid=790b7611yhpb7okmewi6t6acgri4cmf0q3hw6iyfbuw0jgnw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
