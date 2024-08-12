d84d265dc44c9985619dfd4ae5400a9f77b9dea8
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2024-08-11 23:45:26 -0700

    test(config.yml): add lorem text + enable the cache invalidation step to test CloudFront service

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ export const App = () => {
   return (
     <div className="page">
       <h1>AWS POC Sandbox</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore
+        perferendis nostrum incidunt molestias? Laboriosam blanditiis magni
+        minima quibusdam! Enim vero possimus autem aut qui consequatur odit
+        fugit minus? Maiores.
+      </p>
     </div>
   );
 };
