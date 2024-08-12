f3c38c69134fef8837b6a29f65777f6aa04ecbe4
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2024-08-12 11:13:15 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 export const App = () => {
   return (
     <div className="page">
-      {/* <h1>
+      <h1>
         I know how to configure a CCI rollback job w/ AWS S3 buckets!!! 🥳
       </h1>
       <div>
@@ -11,15 +11,15 @@ export const App = () => {
           src="https://media.giphy.com/media/3o751Z6oPw0C69hKuY/giphy.gif?cid=790b7611nlfa6tz9xxl4y0k4lxr3gjn2s8qbjzrakm2u9ew5&ep=v1_gifs_search&rid=giphy.gif&ct=g"
           alt=""
         />
-      </div> */}
-      <h1>AWS POC Sandbox</h1>
+      </div>
+      {/* <h1>AWS POC Sandbox</h1>
       <div>
         <img
           src="https://media.giphy.com/media/bPCwGUF2sKjyE/giphy.gif?cid=790b7611yhpb7okmewi6t6acgri4cmf0q3hw6iyfbuw0jgnw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
           alt=""
         />
         <div className="caption">Ivan learning how to work with AWS ^</div>
-      </div>
+      </div> */}
     </div>
   );
 };
