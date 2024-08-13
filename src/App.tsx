ef6e6478e9301c99f0a4084ad6c29cf2d5a94c1a
Ivan Ontiveros <49131646+Ivan-Ontiveros@users.noreply.github.com>
2024-08-13 15:04:23 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 export const App = () => {
   return (
     <div className="page">
-      <h1 className="h1">
+      {/* <h1 className="h1">
         I know how to configure a CCI rollback job w/ AWS S3 buckets!!! 🥳
-      </h1>
-      {/* <h1>AWS POC Sandbox</h1> */}
+      </h1> */}
+      <h1>AWS POC Sandbox</h1>
     </div>
   );
 };
